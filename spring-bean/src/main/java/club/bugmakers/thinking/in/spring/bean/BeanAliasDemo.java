98f4b8a987fba51466b7295a3b8fa395363a6c9a
bruce.ouyang <bruce.ouyang@bugmakers.club>
2020-02-18 23:11:15 +0800

    update : add spring-bean bean alias name

---
+++ spring-bean/src/main/java/club/bugmakers/thinking/in/spring/bean/BeanAliasDemo.java
@@ -0,0 +1,23 @@
+package club.bugmakers.thinking.in.spring.bean;
+
+import club.bugmakers.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Bean 别名示例
+ */
+public class BeanAliasDemo {
+
+    public static void main(String[] args) {
+
+        // 启动应用上下文
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/bean-definition-context.xml");
+
+        // 通过别名 bruce-user 获取 user 的 bean
+        User bruce_user = beanFactory.getBean("bruce-user", User.class);
+        User user = beanFactory.getBean("user", User.class);
+
+        System.out.println("bruce-user 是否与 user 相同：" + (bruce_user == user));
+    }
+}
