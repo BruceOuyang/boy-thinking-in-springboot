ef33bb8114d7ba36a7623140bf208c19cb6422b8
bruce.ouyang <bruce.ouyang@bugmakers.club>
2020-02-23 10:16:38 +0800

    update : add bean的延迟初始化

--- spring-bean/src/main/java/club/bugmakers/thinking/in/spring/bean/definition/BeanInitializationDemo.java
+++ spring-bean/src/main/java/club/bugmakers/thinking/in/spring/bean/definition/BeanInitializationDemo.java
@@ -5,6 +5,7 @@ import club.bugmakers.thinking.in.spring.bean.factory.UserFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 /**
  * Bean 初始化 demo
@@ -27,14 +28,20 @@ public class BeanInitializationDemo {
         // 启动 Spring 应用上下文
         applicationContext.refresh();
 
+        // 非延迟初始化在 Spring 应用上下文启动完成后，被初始化
+        System.out.println("Spring 应用上下文已启动...");
+
         // 依赖查找 UserFactory
         UserFactory userFactory = applicationContext.getBean(UserFactory.class);
+
+        System.out.println(userFactory);
 
         // 关闭 Spring 应用上下文
         applicationContext.close();
     }
 
     @Bean(initMethod = "initUserFactory")
+    @Lazy(value = false)
     public UserFactory userFactory() {
         return new DefaultUserFactory();
     }
