5a1081d7aa24c68092c883b069a4b661efdbea2a
bruce.ouyang <bruce.ouyang@bugmakers.club>
2020-02-28 22:18:07 +0800

    update : bean的销毁

--- spring-bean/src/main/java/club/bugmakers/thinking/in/spring/bean/definition/BeanInitializationDemo.java
+++ spring-bean/src/main/java/club/bugmakers/thinking/in/spring/bean/definition/BeanInitializationDemo.java
@@ -33,8 +33,8 @@ public class BeanInitializationDemo {
 
         // 依赖查找 UserFactory
         UserFactory userFactory = applicationContext.getBean(UserFactory.class);
-
         System.out.println(userFactory);
+        System.out.println("Spring 应用上下文准备关闭");
 
         // 关闭 Spring 应用上下文
         applicationContext.close();
