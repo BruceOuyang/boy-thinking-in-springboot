4bac829a1c8ff7a9ca10d3467a9d5a754195ce0b
bruce.ouyang <bruce.ouyang@bugmakers.club>
2020-03-01 12:16:32 +0800

    add 延迟查找 + 使用 java 8 特性

--- dependency-lookup/src/main/java/club/bugmakers/thinking/in/spring/dependency/lookup/ObjectProviderDemo.java
+++ dependency-lookup/src/main/java/club/bugmakers/thinking/in/spring/dependency/lookup/ObjectProviderDemo.java
@@ -1,8 +1,10 @@
 package club.bugmakers.thinking.in.spring.dependency.lookup;
 
+import club.bugmakers.thinking.in.spring.ioc.overview.domain.User;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 /**
  * 通过 {@link ObjectProvider} 进行依赖查找
@@ -21,9 +23,36 @@ public class ObjectProviderDemo {
 
         // 按照类型查找 beans
         lookupByObjectProvider(applicationContext);
+        lookupIfAvaliable(applicationContext);
+        lookupByStreamOps(applicationContext);
 
         // 关闭应用上下文
         applicationContext.close();
+    }
+
+    /**
+     * 延迟查找 + 使用 java 8 特性
+     * @param applicationContext
+     */
+    private static void lookupByStreamOps(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
+
+//        Iterable<String> stringIterable = objectProvider;
+//        for (String str : stringIterable) {
+//            System.out.println(str);
+//        }
+
+        objectProvider.stream().forEach(System.out::println);
+    }
+
+    /**
+     * 延迟查找
+     * @param applicationContext
+     */
+    private static void lookupIfAvaliable(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<User> beanProvider = applicationContext.getBeanProvider(User.class);
+        User user = beanProvider.getIfAvailable(User::createUser);
+        System.out.println("当前 User 对象：" + user);
     }
 
     /**
@@ -31,8 +60,14 @@ public class ObjectProviderDemo {
      * @return
      */
     @Bean
+    @Primary // 标识为主要的
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @Bean
+    public String message() {
+        return "Message";
     }
 
     private static void lookupByObjectProvider(AnnotationConfigApplicationContext applicationContext) {
