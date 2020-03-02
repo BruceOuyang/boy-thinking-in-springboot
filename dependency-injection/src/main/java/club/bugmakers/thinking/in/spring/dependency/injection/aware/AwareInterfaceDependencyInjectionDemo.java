9cdc45fce32115e6cc760bb81bfc8f0da288fac5
bruce.ouyang <bruce.ouyang@bugmakers.club>
2020-03-02 23:26:38 +0800

    add aware 接口回调注入

---
+++ dependency-injection/src/main/java/club/bugmakers/thinking/in/spring/dependency/injection/aware/AwareInterfaceDependencyInjectionDemo.java
@@ -0,0 +1,46 @@
+package club.bugmakers.thinking.in.spring.dependency.injection.aware;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.Aware;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * 基于 {@link Aware} 接口回调的依赖注入示例
+ */
+public class AwareInterfaceDependencyInjectionDemo implements BeanFactoryAware, ApplicationContextAware {
+
+    private static BeanFactory beanFactory;
+    private static ApplicationContext applicationContext;
+
+    public static void main(String[] args) {
+
+        // 创建 BeanFactory 容器
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
+        // 测试一：注册 Configuration Class (配置类) -> 就是一个 Spring Bean
+        context.register(AwareInterfaceDependencyInjectionDemo.class);
+
+        // 启动应用上下文
+        context.refresh();
+
+        System.out.println(beanFactory == context.getBeanFactory());
+        System.out.println(applicationContext == context);
+
+        // 关闭应用上下文
+        context.close();
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        AwareInterfaceDependencyInjectionDemo.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        AwareInterfaceDependencyInjectionDemo.applicationContext = applicationContext;
+    }
+}
