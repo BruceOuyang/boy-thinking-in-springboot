cffcb749129cbfbb69215f31160cf44abf2aaeac
bruce.ouyang <bruce.ouyang@bugmakers.club>
2020-03-03 00:04:36 +0800

    add 基础类型注入

--- ioc-container-overview/src/main/java/club/bugmakers/thinking/in/spring/ioc/overview/domain/User.java
+++ ioc-container-overview/src/main/java/club/bugmakers/thinking/in/spring/ioc/overview/domain/User.java
@@ -1,5 +1,8 @@
 package club.bugmakers.thinking.in.spring.ioc.overview.domain;
 
+import club.bugmakers.thinking.in.spring.ioc.overview.enums.City;
+import org.springframework.core.io.Resource;
+
 /**
  * 用户类
  */
@@ -8,6 +11,10 @@ public class User {
     Long id;
 
     String name;
+
+    City city;
+
+    Resource configFileLocation;
 
     public Long getId() {
         return id;
@@ -25,11 +32,29 @@ public class User {
         this.name = name;
     }
 
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public Resource getConfigFileLocation() {
+        return configFileLocation;
+    }
+
+    public void setConfigFileLocation(Resource configFileLocation) {
+        this.configFileLocation = configFileLocation;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", city=" + city +
+                ", configFileLocation=" + configFileLocation +
                 '}';
     }
 
---
+++ ioc-container-overview/src/main/java/club/bugmakers/thinking/in/spring/ioc/overview/enums/City.java
@@ -0,0 +1,10 @@
+package club.bugmakers.thinking.in.spring.ioc.overview.enums;
+
+/**
+ * 城市枚举
+ */
+public enum City {
+    Beijing,
+    Shanghai,
+    Hangzhou
+}
