28d98215b41c883ef89839a6e8c81882ee156da5
bruce.ouyang <bruce.ouyang@bugmakers.club>
2020-03-03 22:46:46 +0800

    add 延迟注入，ObjectProvider/ObjectFactory

---
+++ dependency-injection/src/main/java/club/bugmakers/thinking/in/spring/dependency/injection/lazy/LazyAnnotationDependencyInjectionDemo.java
@@ -0,0 +1,63 @@
+package club.bugmakers.thinking.in.spring.dependency.injection.lazy;
+
+import club.bugmakers.thinking.in.spring.dependency.injection.UserHolder;
+import club.bugmakers.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.Resource;
+import java.util.Set;
+
+/**
+ * 基于 Annotation 资源的依赖 字段 注入示例
+ */
+public class LazyAnnotationDependencyInjectionDemo {
+
+    @Autowired
+    private User user; // 实时注入
+
+    @Autowired
+    private ObjectProvider<User> userObjectProvider; // 延迟注入，单一类型
+
+    @Autowired
+    private ObjectFactory<Set<User>> usersObjectFactory; // 延迟注入，集合类型
+
+    public static void main(String[] args) {
+
+        // 创建 BeanFactory 容器
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(applicationContext);
+        String xmlResourcePath = "classpath:/META-INF/dependency-lookup-context.xml";
+        // 加载 XML 资源，解析并且生成 BeanDefinition
+        beanDefinitionReader.loadBeanDefinitions(xmlResourcePath);
+
+        // 测试一：注册 Configuration Class (配置类，代替 XML 文件)
+        applicationContext.register(LazyAnnotationDependencyInjectionDemo.class);
+
+        // 启动应用上下文
+        applicationContext.refresh();
+
+        // 依赖查找 AnnotationDependencyFieldInjectionDemo# Bean
+        LazyAnnotationDependencyInjectionDemo demo = applicationContext.getBean(LazyAnnotationDependencyInjectionDemo.class);
+
+        // 依赖查找并且创建 Bean
+        System.out.println("demo.user = " + demo.user);
+        System.out.println("demo.userObjectProvider = " + demo.userObjectProvider.getObject()); // 集成 ObjectFactory
+        System.out.println("demo.usersObjectFactory = " + demo.usersObjectFactory.getObject());
+
+        demo.userObjectProvider.stream().forEach(System.out::println);
+
+        // 关闭应用上下文
+        applicationContext.close();
+    }
+
+    @Bean
+    public UserHolder userHolder(User user) {
+        return new UserHolder(user);
+    }
+}
