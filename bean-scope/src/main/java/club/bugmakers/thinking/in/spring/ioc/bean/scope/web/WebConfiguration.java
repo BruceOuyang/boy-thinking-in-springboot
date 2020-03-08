e5885302814f2ca9b41229eea0b75945548b99df
bruce.ouyang <bruce.ouyang@bugmakers.club>
2020-03-09 00:30:23 +0800

    add Request 作用域

---
+++ bean-scope/src/main/java/club/bugmakers/thinking/in/spring/ioc/bean/scope/web/WebConfiguration.java
@@ -0,0 +1,18 @@
+package club.bugmakers.thinking.in.spring.ioc.bean.scope.web;
+
+import club.bugmakers.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration {
+
+    @Bean
+    @RequestScope
+    public User user(){
+        return User.createUser();
+    }
+}
---
+++ bean-scope/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2020/3/9
+  Time: 0:09
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello Bean Scope</title>
+</head>
+<body>
+<h1>Hello Bean Scope</h1>
+<h3>current User: ${user.name}</h3>
+<ul>
+    <li>Singleton</li>
+    <li>Prototype</li>
+    <li>Session</li>
+    <li>Request</li>
+    <li>...</li>
+</ul>
+</body>
+</html>
